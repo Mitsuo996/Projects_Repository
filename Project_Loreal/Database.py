8462abdec32f0e5da71c400cafcffde5e9433297
marianagtzbal <mariana.gtz.bal@gmail.com>
2020-11-29 02:21:51 -0600

    Update(4)

--- Project_Loreal/Database.py
+++ Project_Loreal/Database.py
@@ -6,7 +6,15 @@ from Users import User
 class Database(QObject):
     def __init__(self):
         QObject.__init__(self)
-        conn = sqlite3.connect('melanoma.db')
+        self.conn = sqlite3.connect('melanoma.db')
+
+        self.conn.execute('''
+        CREATE TABLE IF NOT EXISTS submits(id INTEGER PRIMARY KEY AUTOINCREMENT,
+                              Name text,
+                              Age integer,
+                              Gender text,
+                              Location text,
+                              Diagnosis text);''')
 
     def add_user(self,User):
         patient_name= User.name
@@ -15,7 +23,16 @@ class Database(QObject):
         location = User.location
         diagnosis = User.result
 
-        conn.execute("INSERT into submits(Name,Age,Gender,Location,Diagnosis) values (?,?,?,?,?)", (patient_name,age,gender,location,diagnosis))
-        conn.commit()
+        self.conn.execute("INSERT into submits(Name,Age,Gender,Location,Diagnosis) values (?,?,?,?,?)", (patient_name,age,gender,location,diagnosis))
+        self.conn.commit()
+
+        cursor = self.conn.execute(''' SELECT *
+                                  FROM submits;''')
+        for row in cursor:
+          print(row)
+        self.conn.close()
+
+    #def search_user(self):
+
 
 
--- Project_Loreal/main.py
+++ Project_Loreal/main.py
@@ -6,9 +6,10 @@ from PyQt5.QtGui  import QGuiApplication
 from PyQt5.QtQml  import QQmlApplicationEngine
 from PyQt5.QtCore import QObject, pyqtSignal, pyqtSlot, QUrl
 
-from Users import Users
+from Users import Users,User
 from Model import Model
 from Webcam import Webcam
+from Database import Database
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
@@ -17,12 +18,15 @@ if __name__ == "__main__":
     engine = QQmlApplicationEngine()
     Users  = Users()
     webcam = Webcam()
+    database = Database()
     model  = Model(Users,webcam)
     model.load_model()
+    database.add_user(User())
 
     engine.rootContext().setContextProperty("user", Users)
     engine.rootContext().setContextProperty("webcam",webcam)
     engine.rootContext().setContextProperty("app_model",model)
+    engine.rootContext().setContextProperty("database",database)
 
     engine.load(QUrl('main.qml'))
 
